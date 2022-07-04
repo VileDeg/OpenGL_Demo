8ec031387e78b4615d9f10470fdb506a9a47e183
Vadim <gvadim134@gmail.com>
2022-07-04 19:18:21 +0200

    test

--- OpenGL_Demo/src/Application.cpp
+++ OpenGL_Demo/src/Application.cpp
@@ -40,6 +40,7 @@ int main()
         context.PollEvents();
         context.UpdateWindows();
 
+
         context.SetClearColors(0.049f, 0.0f, 0.1f, 1.f);
         context.Clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
